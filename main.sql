006f440fdcadb8ebb2a0c07186c864cc07dd1949
Federico Razzoli <federico.razzoli@humanstate.com>
2018-11-14 21:46:34 +0000

    get_current_trx_id, record_current_trx_id

---
+++ main.sql
@@ -0,0 +1,52 @@
+-- This file can be executed on any reasonably recent version of MySQL or MariaDB.
+-- Some of the routines may be overwritten when executing more specific files.
+
+CREATE DATABASE IF NOT EXISTS _;
+USE _;
+
+DELIMITER ||
+
+DROP FUNCTION IF EXISTS get_current_trx_id;
+CREATE FUNCTION get_current_trx_id()
+    RETURNS BIGINT UNSIGNED
+    NOT DETERMINISTIC
+    READS SQL DATA
+    COMMENT 'Return the id of current transaction'
+BEGIN
+    RETURN (
+        SELECT TRX_ID
+            FROM information_schema.INNODB_TRX
+            WHERE TRX_MYSQL_THREAD_ID = CONNECTION_ID()
+    );
+END ||
+
+DROP TABLE IF EXISTS trx_history;
+CREATE TABLE trx_history (
+    trx_id BIGINT UNSIGNED NOT NULL,
+    trx_timestamp TIMESTAMP(6) NOT NULL,
+    event VARCHAR(50) NOT NULL,
+    PRIMARY KEY (trx_id, trx_timestamp)
+)
+    ENGINE InnoDB,
+    COMMENT 'History of transaction ids and time'
+||
+
+DROP PROCEDURE IF EXISTS record_current_trx_id;
+CREATE PROCEDURE record_current_trx_id(
+        IN in_event VARCHAR(50),
+        OUT out_trx_id BIGINT UNSIGNED
+    )
+    MODIFIES SQL DATA
+    COMMENT 'Write the id of current transaction into _.trx_history'
+BEGIN
+    DECLARE v_trx_id BIGINT UNSIGNED
+        DEFAULT _.get_current_trx_id();
+    INSERT INTO trx_history
+            (trx_id, trx_timestamp, event)
+        VALUES
+            (v_trx_id, NOW(6), in_event);
+    SET out_trx_id := v_trx_id;
+END ||
+
+DELIMITER ;
+
---
+++ test/test_main.sql
@@ -0,0 +1,31 @@
+USE _;
+
+-- test _.get_current_trx_id()
+CREATE TABLE t (x INT UNSIGNED NOT NULL) ENGINE InnoDB;
+START TRANSACTION;
+INSERT INTO t VALUES (1);
+SELECT 'Expect: id' AS message;
+SELECT _.get_current_trx_id();
+COMMIT;
+DROP TABLE t;
+SELECT 'Expect: NULL' AS message;
+SELECT _.get_current_trx_id();
+
+-- test _.record_current_trx_id()
+CREATE TABLE t (x INT UNSIGNED NOT NULL) ENGINE InnoDB;
+START TRANSACTION;
+INSERT INTO t VALUES (1);
+CALL _.record_current_trx_id('Test', @trx_id);
+SELECT 'Expect: 1 row' AS message;
+SELECT @last_trx_id := trx_id, trx_timestamp, event
+    FROM _.trx_history
+    ORDER BY trx_timestamp DESC
+    LIMIT 1
+;
+SELECT 'Expect: 1' AS message;
+SELECT @trx_id = @last_trx_id;
+-- cleanup
+SET @trx_id := NULL;
+SET @last_trx_id := NULL;
+COMMIT;
+
