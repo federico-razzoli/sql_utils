f5a8f2ef7c6318c81928f6610dfb68167cf1cbc5
Federico Razzoli <federico.razzoli@humanstate.com>
2018-11-18 20:45:54 +0000

    Support BIGINT sysver tables in add_system_versioning()

--- mariadb_10-03.sql
+++ mariadb_10-03.sql
@@ -9,20 +9,28 @@ CREATE PROCEDURE add_system_versioning(
     in_database VARCHAR(64),
     in_table VARCHAR(64),
     in_start_column VARCHAR(64),
-    in_end_column VARCHAR(64)
+    in_end_column VARCHAR(64),
+    in_type ENUM('time', 'trx')
 )
     MODIFIES SQL DATA
     COMMENT 'Make an existing table system-versioned'
 BEGIN
-    DECLARE alter_table TEXT;
-    SET alter_table := CONCAT('
+    DECLARE v_alter_table TEXT DEFAULT NULL;
+    DECLARE v_type TEXT DEFAULT NULL;
+SET in_type := UPPER(in_type);
+    IF in_type LIKE 'TIME%' THEN
+        SET v_type := 'TIMESTAMP(6)';
+    ELSEIF in_type IN ('TRX', 'TRANSACTION') THEN
+        SET v_type := 'BIGINT UNSIGNED';
+    END IF;
+    SET v_alter_table := CONCAT('
     ALTER TABLE ', _.quote_name2(in_database, in_table), '
-        ADD COLUMN ',  in_start_column , ' TIMESTAMP(6) GENERATED ALWAYS AS ROW START,
-        ADD COLUMN ',  in_end_column   , ' TIMESTAMP(6) GENERATED ALWAYS AS ROW END,
+        ADD COLUMN ',  in_start_column , ' ', v_type, ' GENERATED ALWAYS AS ROW START,
+        ADD COLUMN ',  in_end_column   , ' ', v_type, ' GENERATED ALWAYS AS ROW END,
         ADD PERIOD FOR SYSTEM_TIME(', in_start_column, ', ', in_end_column, '),
         ADD SYSTEM VERSIONING
     ;');
-    CALL run_sql(alter_table);
+    CALL run_sql(v_alter_table);
 END ||
 
 DELIMITER ;
--- test/test_mariadb_10-03.sql
+++ test/test_mariadb_10-03.sql
@@ -1,11 +1,23 @@
 USE _;
 
-CREATE TABLE IF NOT EXISTS test_table (x INT UNSIGNED NOT NULL) ENGINE InnoDB;
-CALL _.add_system_versioning('_', 'test_table', 'valid_from', 'valid_to');
+-- test _.add_system_versioning()
+CREATE OR REPLACE TABLE IF NOT EXISTS test_table (x INT UNSIGNED NOT NULL) ENGINE InnoDB;
+CALL _.add_system_versioning('_', 'test_table', 'valid_from', 'valid_to', 'time');
 INSERT INTO test_table (x) VALUES (1);
 DELETE FROM test_table WHERE x = 1;
 SELECT
         '<timestamp(6)>,<timestamp(6)>,1' AS expect,
+        valid_from,
+        valid_to,
+        x
+    FROM test_table FOR SYSTEM_TIME ALL
+;
+CREATE OR REPLACE TABLE test_table (x INT UNSIGNED NOT NULL) ENGINE InnoDB;
+CALL _.add_system_versioning('_', 'test_table', 'valid_from', 'valid_to', 'trx');
+INSERT INTO test_table (x) VALUES (1);
+DELETE FROM test_table WHERE x = 1;
+SELECT
+        '<bigint>,<bigint>,1' AS expect,
         valid_from,
         valid_to,
         x
